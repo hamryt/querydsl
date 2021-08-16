aef6e50979c4653483ca4cafc69d93c3b38a2a1f
hamryt <rkdl0829@naver.com>
2021-08-16 22:05:52 +0900

    JPAQueryFactory Bean으로 등록, JPA에 Querydsl 실습 첫번째

---
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -0,0 +1,49 @@
+package study.querydsl.repository;
+
+import static study.querydsl.entity.QMember.member;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+
+@Repository
+public class MemberJpaRepository {
+    
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+    
+    public MemberJpaRepository(EntityManager em, JPAQueryFactory jpaQueryFactory){
+        this.em = em;
+        this.queryFactory = jpaQueryFactory;
+    }
+    
+    public void save(Member member){
+        em.persist(member);
+    }
+    
+    public Optional<Member> findById(Long id){
+        Member findMember = em.find(Member.class, id);
+        return Optional.ofNullable(findMember);
+    }
+    
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+            .getResultList();
+    }
+    
+    public List<Member> findAll_Querydsl(){
+        return queryFactory
+            .selectFrom(member)
+            .fetch();
+    }
+    
+    public List<Member> findByUsername(String username){
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+            .setParameter("username", username)
+            .getResultList();
+    }
+}
