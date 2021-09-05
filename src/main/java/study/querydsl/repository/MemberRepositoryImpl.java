ae2f579cd9f5cc37ccb6f59ae11b8cb63757cfdf
hamryt <Email address>
2021-09-05 22:03:05 +0900

    searchComplex 구현 - count 쿼리 따로 보냄

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -6,15 +6,18 @@ import static study.querydsl.entity.QTeam.team;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import javax.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 public class MemberRepositoryImpl implements MemberRepositoryCustom{
     
@@ -47,6 +50,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
      * FetchResult()는 count와 content를 모두 얻게 해준다. 단점은 쉽게 값을 얻을 수 있는 count쿼리도
      * 본 쿼리가 join이 무더기로 있는 등 복잡하면 똑같은 조건을 붙혀서 count 값을 얻는다. 따라서
      * join과 같은 복잡한 연산을 빼고 count값을 따로 얻는 것이 더 성능에 좋을 것이다.
+     *
      * @param condition
      * @param pageable
      * @return
@@ -72,7 +76,6 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
             .limit(pageable.getPageSize())
             .fetch();
         
-        
         long total = queryFactory
             .select(member)
             .from(member)
@@ -87,10 +90,48 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
         return new PageImpl<>(content, pageable, total);
     }
     
+    /**
+     * count쿼리가 생략 가능한 경우
+     * 1. 페이지 시작이면서 컨텐츠 사이가 페이지 사이즈보다 작을 때
+     * 2. 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
+     * @param condition
+     * @param pageable
+     * @return
+     */
+    
     @Override
     public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition,
         Pageable pageable) {
-        return null;
+        List<MemberTeamDto> content = queryFactory
+            .select(new QMemberTeamDto(
+                member.id,
+                member.username,
+                member.age,
+                team.id,
+                team.name))
+            .from(member)
+            .leftJoin(member.team, team)
+            .where(
+                usernameEq(condition.getUsername()),
+                teamNameEq(condition.getTeamName()),
+                ageGoe(condition.getAgeGoe()),
+                ageLoe(condition.getAgeLoe()))
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+    
+        JPAQuery<Member> countQuery = queryFactory
+            .select(member)
+            .from(member)
+            .leftJoin(member.team, team)
+            .where(
+                usernameEq(condition.getUsername()),
+                teamNameEq(condition.getTeamName()),
+                ageGoe(condition.getAgeGoe()),
+                ageLoe(condition.getAgeLoe())
+            );
+    
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
     }
     
     private Predicate ageLoe(Integer ageLoe) {
